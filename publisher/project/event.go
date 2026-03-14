cd4bf7852a0d3ac5d04ead7514c5153a4e0ff60e
Andrey <akhrtnv@gmail.com>
2026-03-14 09:18:59 +0300

    update project events struct

--- publisher/project/event.go
+++ publisher/project/event.go
@@ -2,7 +2,6 @@ package project
 
 import (
 	"github.com/akemoon/crowdfunding-app-project/domain"
-	"github.com/google/uuid"
 )
 
 type EventType int
@@ -13,7 +12,6 @@ const (
 )
 
 type Event struct {
-	EventID uuid.UUID      `json:"eventID"`
 	Type    EventType      `json:"type"`
 	Project domain.Project `json:"project"`
 }
