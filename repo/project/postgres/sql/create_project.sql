39f737403baad67a701291c792ed91719bed8bb9
Andrey <akhrtnv@gmail.com>
2026-03-30 07:32:19 +0300

    change formatting

--- repo/project/postgres/sql/create_project.sql
+++ repo/project/postgres/sql/create_project.sql
@@ -1,3 +1,20 @@
-insert into projects (user_id, category_id, name, description, currency_id, goal_amount, duration_days)
-values ($1, $2, $3, $4, $5, $6, $7)
-returning id, user_id, category_id, name, description, currency_id, goal_amount, started_at, duration_days, status_id;
+insert into projects (
+    user_id,
+    category_id,
+    name,
+    description,
+    currency_id,
+    goal_amount,
+    duration_days
+) values ($1, $2, $3, $4, $5, $6, $7)
+returning
+    id,
+    user_id,
+    category_id,
+    name,
+    description,
+    currency_id,
+    goal_amount,
+    started_at,
+    duration_days,
+    status_id;
